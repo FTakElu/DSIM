12c5e7c594a6a8c14e712ea1ed6f1d7787f1d8e7
Bagmor <gabriella.pereira.morais@gmail.com>
2025-11-03 01:25:56 -0300

    Update tipos de Dados dos Pacientes

--- Desenvolvimento/3.Implementação/DSIM-COD/frontend/src/Types/PacientesType.ts
+++ Desenvolvimento/3.Implementação/DSIM-COD/frontend/src/Types/PacientesType.ts
@@ -3,30 +3,18 @@ export type VitalStatus = 'stable' | 'warning' | 'danger';
 
 export interface Vital {
   value: number | string;
-<<<<<<< HEAD
   status: VitalStatus; 
 }
 
 export interface Pacientes {
   id: string; 
-=======
-  status: VitalStatus;
-}
-
-export interface Pacientes {
-  id: number;
->>>>>>> 6cb3298850116a63aa43f5d8d8b2feabfc52c032
   nome: string;
   imageUrl: string;
   dataNascimento: string;
   genero: string;
   relacionamento: string;
   telefone: string;
-<<<<<<< HEAD
   contatoEmergencia: {
-=======
-  contatoEmergencial: {
->>>>>>> 6cb3298850116a63aa43f5d8d8b2feabfc52c032
     nome: string;
     telefone: string;
     email: string;
@@ -38,16 +26,9 @@ export interface Pacientes {
     ProblemaEspecifico: string;
   };
   vitals: {
-<<<<<<< HEAD
     oxigenio: Vital;
     temperatura: Vital;
     batimentos: Vital;
   };
 
-=======
-    oxegenio: Vital;
-    temperatura: Vital;
-    batimentos: Vital;
-  };
->>>>>>> 6cb3298850116a63aa43f5d8d8b2feabfc52c032
 }
